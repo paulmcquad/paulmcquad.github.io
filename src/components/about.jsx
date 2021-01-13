8dfe66c4b5a8d458b196ef1957d164b1a69cd5d7
Paul McQuade <paulmcquad@gmail.com>
2021-01-13 23:24:40 +0000

    Updated my Portfolio
    V0.2.0
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ src/components/about.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I'm a electronics and computer science graduate from ITDublin and Roslyn Park. I have a willingness to learn new technologies.</p>
+                    <p>I have been to Málaga - Spain, Rock of Gibraltar, London and New York. I would love to travel more and maybe see Paris and Berlin. </p>
+                    <p>I'm a social coder on github and gitlab which is focused on programming languages, DataScience, Big Data and coding competitions like ProjectEuler.net.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building websites and using firefox and chrome extensions.
+                       I love to use Open Source Software.</p>
+                     <p>I have learned JavaScript, React, Vue, HTML, CSS.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Science & Kaggle</h3>
+                    <p>I have experience working with Data and have learned from kaggle about Big Data.
+                       I have looked at some of kaggle's Datasets.
+                    </p>
+                    <p>I have learned Python, Jupyter and R.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops & CI/CD</h3>
+                    <p>I have expert knowledge about Linux, MacOS, Unix and Windows.
+                      I can work in the command line. 
+                     </p>
+                    <p>I have worked with tools like Git, GitHub and Gitlab.</p>
+                </div>
+                </div>
+            </div>
+            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}

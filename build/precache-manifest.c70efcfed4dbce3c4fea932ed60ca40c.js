0e5aed0bed1d8a43818dd78439a0857b5f5eedb3
Paul McQuade <paulmcquad@gmail.com>
2021-01-14 14:52:24 +0000

    Create Build folder
    v0.2.1
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ build/precache-manifest.c70efcfed4dbce3c4fea932ed60ca40c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be42eb198b79770f551",
+    "url": "/static/css/main.4a207c74.chunk.css"
+  },
+  {
+    "revision": "2be42eb198b79770f551",
+    "url": "/static/js/main.2be42eb1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c0a5ae5f686d1edfb75d25fbb53bc986",
+    "url": "/index.html"
+  }
+];
